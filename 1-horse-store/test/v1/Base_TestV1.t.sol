bca7d62682677c6b105603ef38d4c21457bb9c9e
panditdhamdhere <dhamdherepandit@gmail.com>
2024-03-28 23:14:49 +0530

    tested huff contract

---
+++ 1-horse-store/test/v1/Base_TestV1.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.25;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {HorseStore} from "../../src/horseStoreV1/HorseStore.sol";
+
+abstract contract Base_TestV1 is Test {
+    HorseStore public horseStore;
+
+    function setUp() public virtual {
+        horseStore = new HorseStore();
+    }
+
+    function testReadValue() public {
+        uint256 initialValue = horseStore.readNumberOfHorses();
+        assertEq(initialValue, 0);
+    }
+}
