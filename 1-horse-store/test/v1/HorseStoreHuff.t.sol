2bd94ea964833a6ca3d4f24b3c7e0e2dea9cc440
panditdhamdhere <dhamdherepandit@gmail.com>
2024-03-30 09:05:37 +0530

    testing

--- 1-horse-store/test/v1/HorseStoreHuff.t.sol
+++ 1-horse-store/test/v1/HorseStoreHuff.t.sol
@@ -1,10 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.25;
-import {Base_TestV1} from "./Base_TestV1.t.sol";
+import {Base_TestV1, HorseStore} from "./Base_TestV1.t.sol";
+
+import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
 
 contract HorseStoreHuff is Base_TestV1 {
+    string public constant HORSE_STORE_HUFF_LOCATION =
+        "horseStoreV1/HorseStore";
+
     function setUp() public override {
         // horseStore = huff edition
-        
+        horseStore = HorseStore(
+            HuffDeployer.config().deploy(HORSE_STORE_HUFF_LOCATION)
+        );
     }
 }
